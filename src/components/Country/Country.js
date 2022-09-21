5707bd80e99fb66c5a6d7720b810cf8cf9fd471b
Mahamodul Hasan Moon <coddermoon@gmail.com>
2022-09-21 09:01:38 +0600

    cpuntry loaded done

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -3,12 +3,12 @@ import "./country.css"
 
 const Country = (props) => {
    
-   const {name,population,area} = props.info
+   const {name,population,area,flags} = props.info
 
     return (
         <div className='country'>
          
-
+<img src={flags.png} alt="" />
            <h2>Name :{name.common}</h2>
            <h4>population:{population}</h4>
            <small>area : {area}</small>
